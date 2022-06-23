1a68b54e90b99c82cf857a1842a928b2c4289152
Ajay <ajay@gpr.com>
2022-06-23 09:01:10 -0400

    plotted the 20 images

--- CV-Ex2-Visulization/visualization.py
+++ CV-Ex2-Visulization/visualization.py
@@ -1,5 +1,6 @@
 from utils import get_data
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
 def viz(ground_truth):
@@ -9,9 +10,25 @@ def viz(ground_truth):
     - ground_truth [list[dict]]: ground truth data
     """
     # IMPLEMENT THIS FUNCTION
+    images = []
+    for g in ground_truth:
+        print(g['filename'])
+        images.append('./data/images/'+g['filename'])
 
-    fig, ax = plt.subplots(2,1)
+
+    # fig, ax = plt.subplots(2,1)
+    # plt.show()
+    nrow = 4
+    ncol = 5
+
+
+    _, axs = plt.subplots(nrow, ncol, figsize=(100, 100))
+    axs = axs.flatten()
+    for img, ax in zip(images, axs):
+        # img1 = mpimg(img)
+        ax.imshow(mpimg.imread(img))
     plt.show()
+
     print('Debug Stop')
 
 if __name__ == "__main__": 
